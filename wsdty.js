ac5c09b92018a107686f4bb4ca10030f864660ef
adrianoelguapo <ex8zari@gmail.com>
2025-03-10 04:21:13 +0100

    Página Álbum 1 (wsdty)

---
+++ wsdty.js
@@ -0,0 +1,140 @@
+Amplitude.init({
+    "songs": [
+      {
+        "name": "Track 1: We Still Don't Trust You",
+        "artist": "Future",
+        "url": "audio/we-still-dont-trust-you.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 2: Drink N Dance",
+        "artist": "Future",
+        "url": "audio/drink-and-dance.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 3: Out of My Hands",
+        "artist": "Future",
+        "url": "audio/Out Of My Hands.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 4: Jealous",
+        "artist": "Future",
+        "url": "audio/Jealous.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 5: This Sunday",
+        "artist": "Future",
+        "url": "audio/This Sunday.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 6: Luv Bad Bitches",
+        "artist": "Future",
+        "url": "audio/Luv Bad Bitches.mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 7: Amazing (Interlude)",
+        "artist": "Future",
+        "url": "audio/Amazing (Interlude).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 8: All to Myself",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin, The Weeknd - All to Myself (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 9: Nights Like This",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Nights Like This (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },
+      {
+        "name": "Track 10: Came to the Party",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Came to the Party (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 11: Right 4 You",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Right 4 You (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 12: Mile High Memories",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Mile High Memories (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 13: Overload",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Overload (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 14: Gracious",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Gracious (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 15: Beat It",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Beat It (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 16: Always Be My Fault",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin, The Weeknd - Always Be My Fault (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 17: One Big Family",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - One Big Family (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 18: Red Leather",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Red Leather (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 19: #1",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - #1 (Intro) (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 20: Nobody Knows My Struggle",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Nobody Knows My Struggle (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 21: All My Life",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - All My Life (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 22: Crossed Out",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Crossed Out (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 23: Crazy Clientele",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Crazy Clientele (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 24: Show Of Hands",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin, A$AP Rocky - Show of Hands (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      },{
+        "name": "Track 25: Streets Made Me A King",
+        "artist": "Future",
+        "url": "audio/Future, Metro Boomin - Streets Made Me A King (Official Audio).mp3",
+        "cover_art_url": "images/wsdty.jpg"
+      }
+    ]
+  });
+  
